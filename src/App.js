c2fa18d9cfab3bbeb95889e33933f3de48f4b5b9
ahmed yassine hamrouni <ahmedyassine.hamrouni@isimg.tn>
2026-07-31 02:30:28 +0000

    redesign dark corporate theme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './App.css';
+import './app.css';
 import Login from './components/Login';
 import Signup from './components/signup';
 import Navbar from './components/Navbar';
